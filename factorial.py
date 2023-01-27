a203caa6e402e3b176a761f05f5996d653cfbbed
kenneth leiva <kennleivaa@gmail.com>
2023-01-27 17:04:25 -0600

    tarea factoriales

--- factorial.py
+++ factorial.py
@@ -1,3 +1,7 @@
+#Factoriales de los numeros ingresados 
+
+
+#primero alguna manera recibir el input necesario
 NumEle:int 
 
 while True:
@@ -9,11 +13,18 @@ while True:
     except ValueError:
         print("solo queremos numeros")
 
+#confirmar que el numero es mayor a 0
 if NumEle < 1:
-    print("Error no queremos numeros negativos") 
+    print("Error no queremos numeros negativos")  
 
+#hacer que el programa sea capas de sacar el factorial del numeo dado o input
 factorial=1 
 if NumEle !=0:
     for i in range(1,NumEle+1):
         factorial=factorial*i
-    print("Su factorial es:",factorial)    
+    print("Su factorial es:",factorial)
+
+    
+
+
+
