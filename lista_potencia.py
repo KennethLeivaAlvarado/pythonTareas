e20eb9d7e62c341b9fde77948ac83cf2ed1d609a
kenneth leiva <kennleivaa@gmail.com>
2023-01-28 13:03:31 -0600

    lista de potencias tarea

---
+++ lista_potencia.py
@@ -0,0 +1,5 @@
+#listado de numeros al cubo
+#decidi directamente pasar al proceso de creacion de la lista 
+listInt=[5,9,6]#<---Aqui deben ingresarse los numeros deseados
+for numero in listInt:
+    print(numero **3)
